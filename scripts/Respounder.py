25e68ceea873a353ada9ff2f85cf3804827e725d
Tomas Pelak <s3w3nofficial@gmail.com>
2017-03-07 12:41:09 +0100

    voice changed to female voice

--- scripts/Respounder.py
+++ scripts/Respounder.py
@@ -1,18 +1,10 @@
 import pyttsx
-"""
-class Respounder():
-    def respound(self, say):
-        self.say = say
-        engine = pyttsx.init()
-        engine.say(say)
-        engine.runAndWait()
-"""
+
 class Respounder():
     def respound(self, say):
         self.say = say
         engine = pyttsx.init()
         voices = engine.getProperty('voices')
-        for voice in voices:
-            engine.setProperty('voice', voice.id)
-            engine.say(say)
+        engine.setProperty('voice', voices[1].id)
+        engine.say(say)
         engine.runAndWait()
