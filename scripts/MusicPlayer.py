0743f435d72dfe62bedf7258f4395daddbe56d5a
Tomas Pelak <s3w3nofficial@gmail.com>
2017-03-08 08:51:38 +0100

    music palyer edited

--- scripts/MusicPlayer.py
+++ scripts/MusicPlayer.py
@@ -6,5 +6,12 @@ class MusicPlayer():
         self.sound = sound
         print "playing song: " + sound
         playsound(sound)
-    
+    def play_all(self):
+        stack = OS_PathManager().get_music_path()
+        for s in stack:
+            print s
+        for s in stack:
+            playsound(s)
+  
 #MusicPlayer().play_music("C:\Users\kyber\Desktop\Koneko-chan\Koneko-Chan\scripts\TEST.mp3")
+MusicPlayer().play_all()
--- scripts/OS_PathManager.py
+++ scripts/OS_PathManager.py
@@ -1,3 +1,20 @@
+import os
+
 class OS_PathManager():
     def get_music_path(self):
-        pass
+        files = os.listdir("D:")
+        print len(files)
+        i = len(files) - 1
+        while i != -1:
+            if "mp3" not in files[i]:
+                files.pop(i)
+            else:
+                a = files[i]
+                files[i] = "D:\\" + a
+                print a
+            i -= 1
+        for file in files
+        print len(files)
+        return files
+
+OS_PathManager().get_music_path()   
